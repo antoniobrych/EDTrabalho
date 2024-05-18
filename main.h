e6ad5b5c3c2c7578f08a448a7c9c8f2830d719bf
Jhonny <carvalhoborgeshenrique@gmail.com>
2024-05-18 16:01:35 -0300

    update header

--- main.h
+++ main.h
@@ -6,5 +6,21 @@
 #define __EDTRABALHO_MAIN_H_
 
 
+// Struct
+// Node and your functions
+
+typedef struct Node
+{
+    int iPaySload;
+    Node* ptrNext;
+    Node* ptrPrev;
+}Node;
+
+Node * createNode(int);
+void displayList(Node*);
+
+// Sort Methods
+
+
 
 #endif //EDTRABALHO_MAIN_H
