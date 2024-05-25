3c83dafed7b387bf33f64a6de3a6b46f960c9d0f
Jhonny <carvalhoborgeshenrique@gmail.com>
2024-05-25 17:04:54 -0300

    Adicionando Shellsort

--- Working With Header/main.cpp
+++ Working With Header/main.cpp
@@ -12,27 +12,77 @@ void generateRandomDLL(Node** head, int numNodes)
         insertEnd(head, payload);
     }
 }
+void displayarray(int a[],int siz);
 
 int main()
 {
+    int a[] = {3,0,1,8,7,2,5,4, 9, 6};
+
     cout << "Using ShellSort: " << endl;
     Node* head = nullptr;
-    generateRandomDLL(&head, 30);
+    generateRandomDLL(&head, 10);
     displayList(head);
     cout << endl;
     cout << "Sorted by ShellSort: " << endl;
     shellSort(&head);
     displayList(head);
-
-
+    cout << "Vamos fazer um caso de exemplo: https://www.youtube.com/watch?v=yn0EgXHb5jc " << endl;
+    cout << "Considere a seguinte lista: ";
+    for (int i = 0 ; i < 10 ; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    cout << "Vamos fazer a primeira iteração:" << endl;
+    cout << "Encontramos o size da lista: 10" << endl;
+    cout << "Temos o gap: size/2 --> gap = 10/2 " << endl;
+    cout << "Fazemos a comparação do a[gap], a[5] com o a[0] " << endl;
+    cout << "nesse caso, 2 < 3, então trocam de posição." << endl;
+    cout << "E proseguimos fazendo as comparações a[5+1] a[0+1], e depois a[5+2] a[0+2] até chegar ao a[9] a[4]"<< endl;
+    for (int j = 5 ; j <= 9 ; j ++)
+    {
+        if(a[j] < a[j-5])
+        {
+            int temp = a[j];
+            a[j] = a[j-5];
+            a[j-5] = temp;
+        }
+        displayarray(a,10);
+        cout << endl;
+    }
+    cout << "Vemos que fora alterado apenas na primeira e na ultima iteracao."<< endl;
+    cout << "Agora atualizamos o gap. Gap = 5/2, nesse caso usamos gap = 3" << endl;
+    cout << "Fazemos a primeira iteracao: a[3] a[0] e prosseguimos como anteriormente" << endl;
+    for (int k = 3; k<=9; k++)
+    {
+        if(a[k] < a[k - 3])
+        {
+            int temp = a[k];
+            a[k] = a[k-3];
+            a[k-3] = temp;
+        }
+        displayarray(a, 10);
+        cout << endl;
+    }
+    cout << " E então, novamente reduzimos o gap pra 1" << endl;
+    for (int k = 1; k<=9; k++)
+    {
+        if(a[k] < a[k - 1])
+        {
+            int temp = a[k];
+            a[k] = a[k-1];
+            a[k-1] = temp;
+        }
+        displayarray(a, 10);
+        cout << endl;
+    }
     return 0;
 }
 
-//
-//  1 , 2 ,5 , 4 , 3 , 6,
-// h = 4
-// [ 1 , 4] [2, 3] [5 , 6]
-// h = 2
-// [1 , 2] [2 , 5] [5 ,4] [4,3] [3 ,6]
-//
-//
+void displayarray(int a[],int siz)
+{
+    for (int i = 0; i < siz;i++)
+    {
+        cout << a[i] << " ";
+    }
+}
